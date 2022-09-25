949bc0608a475dd9e0aa739929ae574efaa63d5a
DPrandzhev <114281603+DPrandzhev@users.noreply.github.com>
2022-09-25 13:02:57 +0300

    update

---
+++ Programming-Fundamentals-Softuni-Python/Data Types and Variables/chars_to_string.py
@@ -0,0 +1,2 @@
+first_input, second_input, third_input = input(), input(), input()
+print(f"{first_input}{second_input}{third_input}")
--- Programming-Fundamentals-Softuni-Python/Data Types and Variables/integer_iperations.py
+++ Programming-Fundamentals-Softuni-Python/Data Types and Variables/integer_iperations.py
@@ -1,1 +1,5 @@
-print("hello")
+num_one, num_two, num_tree, num_four = int(input()), int(input()), int(input()), int(input())
+
+sum_numbers = int((num_one + num_two) / num_tree) * num_four
+
+print(sum_numbers)
